cc22498431bd87610b2bd47f51da9c91e728123d
Tiziano <139075731+roloar7@users.noreply.github.com>
2026-04-24 15:21:44 -0300

    react router

--- Tienda/src/components/images.jsx
+++ Tienda/src/components/images.jsx
@@ -1,16 +1,16 @@
 // Importamos las imágenes desde la carpeta local
-import zapatillas from './assets/images/zapatillas.jpeg';
-import essentialPantalon from './assets/images/Essentials-Pantalon.jpg';
-import Gorras from './assets/images/Gorras.jpeg';
-import buzos from './assets/images/buzos.jpeg';
-import remeras from './assets/Images/remeras.jpg';
-import cinturones from './assets/Images/cinturones.jpg';
-import bolsos from './assets/images/bolsos.jpeg';
-import accesorios from './assets/images/accesorios.jpg';
-import noctanegro from './assets/Images/noctanegro.png'
-import shortvalley from './assets/images/shortvalley.png';
-import corteizsupreme from './assets/images/corteizsupreme.png';
-import burberry from './assets/images/burberry1.png';
+import zapatillas from '../assets/images/zapatillas.jpeg';
+import essentialPantalon from '../assets/images/Essentials-Pantalon.jpg';
+import Gorras from '../assets/images/Gorras.jpeg';
+import buzos from '../assets/images/buzos.jpeg';
+import remeras from '../assets/Images/remeras.jpg';
+import cinturones from '../assets/Images/cinturones.jpg';
+import bolsos from '../assets/images/bolsos.jpeg';
+import accesorios from '../assets/images/accesorios.jpg';
+import noctanegro from '../assets/Images/noctanegro.png'
+import shortvalley from '../assets/images/shortvalley.png';
+import corteizsupreme from '../assets/images/corteizsupreme.png';
+import burberry from '../assets/images/burberry1.png';
 // Las agrupamos en un objeto para exportarlas
 
 const Images = {
