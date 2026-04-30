9b1216782d1800fe831c4891e945fa9bf0939923
Tiziano <139075731+roloar7@users.noreply.github.com>
2026-04-29 23:47:40 -0300

    Sneakers pagina

--- Tienda/src/components/Sneakers/Sneakers.jsx
+++ Tienda/src/components/Sneakers/Sneakers.jsx
@@ -4,11 +4,68 @@ import "./Sneakers.css";
 
 
 const Home = () => {
-    return(
-         <>
-            <img src={Images.prueba} alt="" className='prueba' />
-         </>
-         )
+    return (
+        <>
+            <main className='sneakers-container'>
+                <aside className='asidebar-filter'> </aside>
+                <div className='grid-box'>
+                <div class="parent-grid">
+                    <div class="div1" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                        <p className='titulo-sneakers'>Product Name</p>
+                        <p className='descripcion-sneakers'>Product Description</p>
+                        <p className='precio-sneakers'>$ 99.99</p>
+                    </div>
+                    <div className="div2" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div className="div3" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div className="div4" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div className="div5" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div className="div6" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div className="div7" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div className="div8" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div9" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div10" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div11" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div12" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div13" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div14" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div15" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                    <div class="div16" className='sneakers-card'>
+                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                    </div>
+                </div>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default Home
