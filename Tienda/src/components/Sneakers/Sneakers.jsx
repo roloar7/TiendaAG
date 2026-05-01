939b6d8ada730080a34941a8f5d1e5afe8bacce4
agusgurzi122 <agusgurzi100@gmail.com>
2026-05-01 17:41:35 -0300

    categorais y filtros (sin terminar)

--- Tienda/src/components/Sneakers/Sneakers.jsx
+++ Tienda/src/components/Sneakers/Sneakers.jsx
@@ -5,107 +5,122 @@ const Sneakers = () => {
     return (
         <>
             <main className='sneakers-container'>
-                <aside className='asidebar-filter'> </aside>
+                <aside className='asidebar-filter'>
+                    <div className='title-filtros'><h3>Filtros</h3></div>
+                    <div className='container-categorias'>
+                    <div className='box-categorias'>
+                    <p>Tipo De Prodcucto</p>
+                    </div>
+                    <div className='box-categorias'><p>Marcas</p>
+                    </div>
+                    <div className='box-categorias'><p>Talle</p>
+                    </div>
+                    <div className='box-categorias'><p>Color</p>
+                    </div>
+                    </div>
+                    <p></p>
+
+                </aside>
                 <div className='grid-box'>
-                <div class="parent-grid">
-                    <div class="div1" className='sneakers-card'>
-                        <img src={Images.jordan11} alt="" className='img-sneakers-grid' />
-                        <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
+                    <div class="parent-grid">
+                        <div class="div1" className='sneakers-card'>
+                            <img src={Images.jordan11} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div2" className='sneakers-card'>
+
+                            <img src={Images.jordan15} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div3" className='sneakers-card'>
+                            <img src={Images.jordan3} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div4" className='sneakers-card'>
+                            <img src={Images.jordan3black} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div5" className='sneakers-card'>
+                            <img src={Images.jordan4} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div6" className='sneakers-card'>
+                            <img src={Images.jordan42} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div7" className='sneakers-card'>
+                            <img src={Images.jordanred3} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div className="div8" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div9" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div10" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div11" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div12" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div13" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div14" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div15" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
+                        <div class="div16" className='sneakers-card'>
+                            <img src={Images.prueba} alt="" className='img-sneakers-grid' />
+                            <p className='titulo-sneakers'>Product Name</p>
+                            <p className='descripcion-sneakers'>Product Description</p>
+                            <p className='precio-sneakers'>$ 99.99</p>
+                        </div>
                     </div>
-                    <div className="div2" className='sneakers-card'>
-                       
-                     <img src={Images.jordan15} alt="" className='img-sneakers-grid' />
-                      <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div className="div3" className='sneakers-card'>
-                        <img src={Images.jordan3} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div className="div4" className='sneakers-card'>
-                        <img src={Images.jordan3black} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div className="div5" className='sneakers-card'>
-                        <img src={Images.jordan4} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div className="div6" className='sneakers-card'>
-                        <img src={Images.jordan42} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div className="div7" className='sneakers-card'>
-                        <img src={Images.jordanred3} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div className="div8" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div9" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div10" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div11" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div12" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div13" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div14" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div15" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                    <div class="div16" className='sneakers-card'>
-                        <img src={Images.prueba} alt="" className='img-sneakers-grid' />
-                         <p className='titulo-sneakers'>Product Name</p>
-                        <p className='descripcion-sneakers'>Product Description</p>
-                        <p className='precio-sneakers'>$ 99.99</p>
-                    </div>
-                </div>
                 </div>
             </main>
         </>
